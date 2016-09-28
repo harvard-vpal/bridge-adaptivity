fed98e355f3f4378427980fadbf03a3733fb0662
Andrew Ang <ang.kunanit@gmail.com>
2016-09-28 13:45:32 -0400

    problem_attempt submit function cleanup

--- api/views.py
+++ api/views.py
@@ -27,53 +27,46 @@ def problem_attempt(request):
             'message': 'problem not found for given usage id'
         })
 
-    # construct initial attempt object
-    attempt = Attempt(
+    # create and save the attempt
+    attempt = Attempt.objects.create(
         activity = activity,
         username = request.POST['user'],
         points = float(request.POST['points']),
         max_points = float(request.POST.get('points', 0)), # max_points may not be passed for some questions
     )
-    attempt.save()
 
-    # identify user by looking for user_module, based on edx username and activity.module
-    # if seen before we will have an associated user module
+    # attempt to populate user field on attempt instance
+    # identify user by looking for existing user_module, based on edx username and activity.module
     try: 
-        print "getting user module that corresponds to username {} and module {}".format(request.POST['user'],activity.module.pk)
         user_module = UserModule.objects.get(
             ltiparameters__lis_person_sourcedid = request.POST['user'],
             module = activity.module,
         )
         # set user object on attempt.user
-        attempt.user = user_module.user
+        attempt.update(user = user_module.user)
 
-    # if there is no existing user module, still save attempt instance but take no further action (like posting to tutorgen)
+    # if there is no existing user module, still save attempt instance but take no further action (like posting transaction to activity service)
     except ObjectDoesNotExist:
-        print 'attempt recorded but user, activity.module combo not found'
-        attempt.save()
         return JsonResponse({
             'success':True,
-            'message': 'attempt recorded, but (user, activity.module) combination not recognized',
+            'message': 'attempt recorded, but user_module not identified',
         })
 
-    # submit problem grade info to tutorgen
+    # submit problem grade info to activity service
     transaction = activity_service.Transaction(attempt)
 
     # identify the sequence_item for the activity
     try:
-        print "identifying sequence item for activity"
-        attempt.sequence_item = user_module.sequenceitem_set.get(activity=activity)
-        attempt.save()
-    # what if student is doing problem outside the lti module, before they get to the activity in the module
+        attempt.update(sequence_item = user_module.sequenceitem_set.get(activity=activity))
+
+    # catches a case where student does the problem outside the lti module, before they see the activity in the module
     except ObjectDoesNotExist:
-        attempt.save()
         return JsonResponse({
             'success':True,
-            'message': 'attempt recorded but sequence_item not identifiable',
+            'message': 'attempt recorded and user_module identified, but sequence_item with specified activity not found',
         })
 
     # recompute user_module.grade state and do grade passback
-    print "recomputing grade, doing grade passback"
     user_module.recompute_grade()
     user_module.grade_passback()
 
