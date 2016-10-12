bb07bb310d01958c35ba575e3c845cd6f85e6efb
Andrew Ang <ang.kunanit@gmail.com>
2016-10-12 13:58:52 -0400

    forgot custom_ in front of custom lti parameter name

--- lti/views.py
+++ lti/views.py
@@ -25,7 +25,7 @@ def launch(request):
         raise Http404('custom parameter "module" not specified in custom LTI parameters')
     module_id = int(request.POST['custom_module'])
     module = get_object_or_404(Module,pk=module_id)
-    max_points = float(request.POST['max_points'])
+    max_points = float(request.POST['custom_max_points'])
 
     # create new usermodule object if doesnt exist
     user_module, created = UserModule.objects.get_or_create(
