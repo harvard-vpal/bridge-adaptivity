d490b64e2f3426ae1be77511358ff49969917aab
Andrew Ang <ang.kunanit@gmail.com>
2017-08-07 16:30:55 -0400

    Update import statement for new module name

--- bridge-adaptivity/module/models.py
+++ bridge-adaptivity/module/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Max, Sum
-import lti.utils
+import bridge_lti.utils
 
 class Course(models.Model):
     '''
