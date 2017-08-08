ca52ce9d87f2ad020ca9fb8c951482631f6d0154
wowkalucky <wowkalucky@gmail.com>
2017-08-08 23:51:56 +0300

    Implement Collection list and create views

--- bridge-adaptivity/adaptive_edx/urls.py
+++ bridge-adaptivity/adaptive_edx/urls.py
@@ -2,11 +2,15 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', login_required(RedirectView.as_view(pattern_name='module:collection-list')), name='index'),
     url(r'^lti/', include('bridge_lti.urls', namespace="lti")),
     url(r'^module/', include('module.urls', namespace="module")),
     url(r'^api/', include('api.urls', namespace="api")),
--- bridge-adaptivity/module/urls.py
+++ bridge-adaptivity/module/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls import url, include
-from . import views
+from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+
+from .views import CollectionList, CollectionCreate
 
 urlpatterns = [
-	url(r'^launch/(?P<user_module_id>[0-9]+)$', views.launch, name='launch'),
-    url(r'^(?P<user_module_id>[0-9]+)/(?P<position>[0-9]+)$', views.sequence_item, name='sequence_item'),
-	url(r'^(?P<user_module_id>[0-9]+)/(?P<position>[0-9]+)/next_activity$', views.next_activity, name='next_activity'),
-	url(r'^sequence_complete/(?P<user_module_id>[0-9]+)$', views.sequence_complete, name='sequence_complete'),
-	url(r'^completion_message$', views.completion_message, name='completion_message'),
+    url(r'^collection/$', login_required(CollectionList.as_view()), name='collection-list'),
+    url(r'^collection/add/$', CollectionCreate.as_view(), name='collection-add'),
 ]
-
