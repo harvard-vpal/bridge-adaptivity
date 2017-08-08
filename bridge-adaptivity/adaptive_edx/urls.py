75c635a390da2a76b85776eca0ab0e3595b9a368
wowkalucky <wowkalucky@gmail.com>
2017-08-08 23:34:01 +0300

    Configure debug toolbar

--- bridge-adaptivity/adaptive_edx/urls.py
+++ bridge-adaptivity/adaptive_edx/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
@@ -12,3 +13,8 @@ urlpatterns = [
     url(r'^api/', include('api.urls', namespace="api")),
     # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [url(r'^__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
