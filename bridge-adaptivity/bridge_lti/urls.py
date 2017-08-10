ce4610035a5371d39b56b935dda14797b1345e97
wowkalucky <wowkalucky@gmail.com>
2017-08-10 17:48:22 +0300

    Add few UI cosmetic changes

--- bridge-adaptivity/bridge_lti/urls.py
+++ bridge-adaptivity/bridge_lti/urls.py
@@ -5,7 +5,7 @@ from .views import LtiSourceList, LtiSourceDetail
 from .consumer import content_source
 
 urlpatterns = [
-    url(r'^launch$', lti_launch, name='lti_provider_launch'),
+    url(r'^launch$', lti_launch, name='lti-provider-launch'),
     url(r'^sources/$', LtiSourceList.as_view(), name='source-list'),
     url(r'^sources/(?P<pk>\d+)/$', LtiSourceDetail.as_view(), name='source-detail'),
     url(r'^content_source(?P<pk>\d+)/$', content_source, name='content-source'),
