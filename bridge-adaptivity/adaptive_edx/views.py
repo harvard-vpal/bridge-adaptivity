db6ebf4ffbe3816726128fa6e909b7df233999f3
wowkalucky <wowkalucky@gmail.com>
2017-08-23 18:29:35 +0300

    Remove deprecated files

--- bridge-adaptivity/adaptive_edx/views.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import PermissionDenied
-from django.http import HttpResponse
-
-def home(request):
-	return HttpResponse('Adaptive edX LTI tool')
-
-def lti_auth_error(request):
-    raise PermissionDenied
-
