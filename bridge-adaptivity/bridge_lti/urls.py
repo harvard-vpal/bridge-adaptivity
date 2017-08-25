b313896e2018c38c905a027d72a20732ab0dea8f
wowkalucky <wowkalucky@gmail.com>
2017-08-25 14:38:34 +0300

    Change initial LTI launch URL of the Bridge

--- bridge-adaptivity/bridge_lti/urls.py
+++ bridge-adaptivity/bridge_lti/urls.py
@@ -5,7 +5,7 @@ from .provider import lti_launch
 from .consumer import source_preview
 
 urlpatterns = [
-    url(r'^launch/$', lti_launch, name='initial-launch'),
+    url(r'^launch$', lti_launch, name='initial-launch'),
     url(r'^launch/(?P<collection_id>\d+)$', lti_launch, name='launch'),
     url(r'^source/$', login_required(source_preview), name='source-preview'),
 ]
