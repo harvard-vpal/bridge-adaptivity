0e063f208dc875dd64c38e744f46a569b697769b
wowkalucky <wowkalucky@gmail.com>
2017-08-29 22:26:24 +0300

    Refactor bridge LTI launch view

--- bridge-adaptivity/bridge_lti/provider.py
+++ bridge-adaptivity/bridge_lti/provider.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from bridge_lti.validator import SignatureValidator
 from module.models import (Collection, Sequence, SequenceItem)
 from .models import LtiProvider, LtiUser
-from .utils import get_required_params, get_optional_params
+from .utils import get_required_params, get_optional_params, LtiRole
 
 
 @csrf_exempt
@@ -31,37 +31,41 @@ def lti_launch(request, collection_id=None):
     if not SignatureValidator(lti_consumer).verify(request):
         return HttpResponseForbidden()
 
-    if params['roles'] in ['Student', 'Learner']:
+    if LtiRole(params['roles']).is_instructor:
         if not collection_id:
-            return render(request, template_name="bridge_lti/announcement.html")
-        try:
-            collection = Collection.objects.get(id=collection_id)
-        except Collection.DoesNotExist:
-            return HttpResponseBadRequest()
+            return redirect(reverse('module:collection-list'))
 
-        lti_user, created = LtiUser.objects.get_or_create(
-            user_id=params['user_id'],
-            lti_consumer=lti_consumer,
-            defaults={'course_id': params['context_id']}
+        return redirect(reverse('module:collection-detail', kwargs={'pk': collection_id}))
+
+    if not collection_id:
+        return render(request, template_name="bridge_lti/announcement.html")
+
+    try:
+        collection = Collection.objects.get(id=collection_id)
+    except Collection.DoesNotExist:
+        return HttpResponseBadRequest()
+
+    lti_user, created = LtiUser.objects.get_or_create(
+        user_id=params['user_id'],
+        lti_consumer=lti_consumer,
+        defaults={'course_id': params['context_id']}
+    )
+    sequence, created = Sequence.objects.get_or_create(
+        lti_user=lti_user,
+        collection=collection
+    )
+
+    if sequence.completed:
+        return redirect(reverse('module:sequence-complete', kwargs={'pk': sequence.id}))
+
+    if created:
+        sequence_item = SequenceItem.objects.create(
+            sequence=sequence,
+            activity=collection.activity_set.first(),
+            position=1
         )
-        sequence, created = Sequence.objects.get_or_create(
-            lti_user=lti_user,
-            collection=collection
-        )
-
-        if sequence.completed:
-            return redirect(reverse('module:sequence-complete', kwargs={'pk': sequence.id}))
-
-        if created:
-            sequence_item = SequenceItem.objects.create(
-                sequence=sequence,
-                activity=collection.activity_set.first(),
-                position=1
-            )
-        else:
-            sequence_item = sequence.sequenceitem_set.last()
-
-        return redirect(reverse('module:sequence-item', kwargs={'pk': sequence_item.id}))
-
     else:
-        return redirect(reverse('module:collection-list'))
+        sequence_item = sequence.sequenceitem_set.last()
+
+    return redirect(reverse('module:sequence-item', kwargs={'pk': sequence_item.id}))
+
