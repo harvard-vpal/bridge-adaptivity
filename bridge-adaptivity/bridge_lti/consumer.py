6e3e1c65f2861cfd3169295a7bbafb27eeb26b4f
wowkalucky <wowkalucky@gmail.com>
2017-08-29 16:54:10 +0300

    Satisfy code review complaints

--- bridge-adaptivity/bridge_lti/consumer.py
+++ bridge-adaptivity/bridge_lti/consumer.py
@@ -10,6 +10,7 @@ from module.models import Activity
 def tool_config(request):
     app_title = 'Bridge'
     app_description = 'Bridge for Adaptivity'
+    launch_view_name = 'lti_launch'  # noqa: F841
     launch_url = request.build_absolute_uri(reverse('lti_launch'))
 
     extensions = {}
