9c96b43a26bc5c745daa60ef3225680e083be37b
Volodymyr Vakulenko <wowkalucky@gmail.com>
2017-09-04 13:42:07 +0300

    Make trailing slash in Bridge LTI launch URLs optional (#19)
    * Make trailing slash in Bridge LTI launch URLs optional
    
    * Update base settings
    
    * Make bridge LTI launch URL less strict
    
    * Add LtiRole constant entity
    
    * Refactor bridge LTI launch view
    
    * Fix code style complaints
    
    * Add BadRequestResponse reason
    
    * Prettify launch url regex
    
    * Move login_required check from urls to view
    
    * Reduce launch view complexity

--- bridge-adaptivity/bridge_lti/urls.py
+++ bridge-adaptivity/bridge_lti/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url
-from django.contrib.auth.decorators import login_required
 
 from .provider import lti_launch
 from .consumer import source_preview
 
 urlpatterns = [
-    url(r'^launch$', lti_launch, name='initial-launch'),
-    url(r'^launch/(?P<collection_id>\d+)$', lti_launch, name='launch'),
-    url(r'^source/$', login_required(source_preview), name='source-preview'),
+    url(r'^launch/(?P<collection_id>\d*)$', lti_launch, name='launch'),
+    url(r'^source/$', source_preview, name='source-preview'),
 ]
