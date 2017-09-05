e4a1b1a309b6b52cd3217b5f295aff532a0539b6
wowkalucky <wowkalucky@gmail.com>
2017-09-05 10:33:46 +0300

    Amend lti launch regex change

--- bridge_adaptivity/bridge_lti/urls.py
+++ bridge_adaptivity/bridge_lti/urls.py
@@ -4,6 +4,6 @@ from .provider import lti_launch
 from .consumer import source_preview
 
 urlpatterns = [
-    url(r'^launch(?P<collection_id>\d*)$', lti_launch, name='launch'),
+    url(r'^launch/(?P<collection_id>\d*)$', lti_launch, name='launch'),
     url(r'^source/$', source_preview, name='source-preview'),
 ]
