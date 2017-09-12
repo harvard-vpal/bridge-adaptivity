2fbbb2f415665aae92571302cfd0e1e51ee3a6b2
wowkalucky <wowkalucky@gmail.com>
2017-09-12 10:31:17 +0300

    Add state object; refactor accordion click handling

--- bridge_adaptivity/module/static/module/js/module.js
+++ bridge_adaptivity/module/static/module/js/module.js
@@ -1,5 +1,39 @@
 (function ($) {
     $(document).ready(function () {
+        var bridgeState = (function() {
+            return {
+                load: function() {
+                    var state = JSON.parse(localStorage.getItem('bridgeState'));
+                    if (state == null) {
+                        state = {
+                            accordion: {
+                                opened: null,
+                                courseData: {}
+                            }
+                        };
+                        console.log("State initialization: ", state.accordion);
+                    } else {
+                        console.log("State from storage: ", state.accordion);
+                        // var $a = $("#accordion")
+                        //     .find($("#accordion a[data-course-index='" + state.accordion.opened + "']"))
+                        //     .trigger('click');
+                        // console.log("Found: ", $a[0])
+                    }
+                    $.extend(this, state);
+                    console.log("State loaded: ", this);
+                },
+
+                save: function(newState) {
+                    var state = {};
+                    $.extend(state, newState);
+                    localStorage.setItem('bridgeState', JSON.stringify(state));
+                    console.log("State saved: ", state);
+                }
+            };
+        }());
+
+        bridgeState.load();
+
         var modalContentFrame = $('#modal-source-preview');
         var typeToIcon = {
             html: "glyphicon-list-alt",
@@ -21,45 +55,68 @@
 
         $("#accordion").find("a[data-course-id]").on('click', function () {
             var target = $(this);
-            var data = {course_id: target.data("course-id")};
-            $.post(internalUrls.apiSources, data, function (data) {
-                var content_panel = target.closest(".panel.panel-default").find(".panel-body").first();
-                if (content_panel.find('ul.content-sources').length) {
-                    return
-                }
-
-                var sourcesList = $('<div/>').addClass('list-group');
-                $.each(data, function (i, item) {
-                    var listItem = $('<button/>')
-                        .addClass('list-group-item')
-                        .attr('type', 'button')
-                        .appendTo(sourcesList);
-                    var sourceIcon = $('<span/>')
-                        .data('toggle', 'tooltip')
-                        .attr('title', item.type)
-                        .addClass("badge pull-left glyphicon " + typeToIcon[item.type])
-                        .text(" ")
-                        .css('margin-right', '5px')
-                        .appendTo(listItem);
-                    var sourceButton = $('<span/>')
-                        .attr('data-toggle', 'modal')
-                        .attr('data-target', '#activityModal')
-                        .text(item['display_name'])
-                        .appendTo(listItem);
-                    sourceButton.on('click', function () {
-                        setInitialActivityData(item);
-                    });
-                    createPreviewButton(
-                        item['display_name'],
-                        item['lti_url'],
-                        item['id'],
-                        listItem,
-                        modalContentFrame
-                    )
-                });
-                content_panel.append(sourcesList);
-            });
+            var activeCourseIndex = target.data('course-index');
+            var activeCourseId = target.data("course-id");
+            clickHandler(activeCourseIndex, activeCourseId, bridgeState);
         });
+
+        function clickHandler(activeCourseIndex, activeCourseId, state) {
+            var courseData;
+            var content_panel = $("#content-panel-" + activeCourseIndex).find(".panel-body").first();
+            if (content_panel.find('ul.content-sources').length) {
+                return
+            }
+
+            if (state.accordion.courseData[activeCourseIndex] !== undefined) {
+                courseData= state.accordion.courseData[activeCourseIndex];
+                renderCourseBlocks(courseData, content_panel);
+            } else {
+                var requestData = {course_id: activeCourseId};
+                $.post(internalUrls.apiSources, requestData, function (responseData) {
+                    console.log("API request...");
+                    // bridgeState.save({
+                    //     accordion: {
+                    //         opened: (activeCourseIndex != bridgeState.accordion.opened) ? activeCourseIndex : null,
+                    //         courseData: {}
+                    //     }.courseData[activeCourseIndex] = responseData
+                    // });
+                    renderCourseBlocks(responseData, content_panel);
+                });
+            }
+        }
+
+        function renderCourseBlocks(courseData, container) {
+            var sourcesList = $('<div/>').addClass('list-group');
+            $.each(courseData, function (i, item) {
+                var listItem = $('<button/>')
+                    .addClass('list-group-item')
+                    .attr('type', 'button')
+                    .appendTo(sourcesList);
+                var sourceIcon = $('<span/>')
+                    .data('toggle', 'tooltip')
+                    .attr('title', item.type)
+                    .addClass("badge pull-left glyphicon " + typeToIcon[item.type])
+                    .text(" ")
+                    .css('margin-right', '5px')
+                    .appendTo(listItem);
+                var sourceButton = $('<span/>')
+                    .attr('data-toggle', 'modal')
+                    .attr('data-target', '#activityModal')
+                    .text(item['display_name'])
+                    .appendTo(listItem);
+                sourceButton.on('click', function () {
+                    setInitialActivityData(item);
+                });
+                createPreviewButton(
+                    item['display_name'],
+                    item['lti_url'],
+                    item['id'],
+                    listItem,
+                    modalContentFrame
+                )
+            });
+            container.append(sourcesList);
+        }
 
         function createPreviewButton(title, ltiUrl, sourceId, parent, modalFrame) {
             var preview = $('<a/>')
