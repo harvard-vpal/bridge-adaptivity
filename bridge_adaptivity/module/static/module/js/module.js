67918fcf2ab8743e30ac28a05450d07b703fc594
wowkalucky <wowkalucky@gmail.com>
2017-09-12 16:10:12 +0300

    Implement persistent collection detail view state

--- bridge_adaptivity/module/static/module/js/module.js
+++ bridge_adaptivity/module/static/module/js/module.js
@@ -1,33 +1,36 @@
 (function ($) {
     $(document).ready(function () {
+        var typeToIcon = {
+            html: "glyphicon-list-alt",
+            problem: "glyphicon-question-sign",
+            video: "glyphicon-facetime-video",
+            other: "glyphicon-option-horizontal"
+        };
         var bridgeState = (function() {
             return {
                 load: function() {
-                    var state = JSON.parse(localStorage.getItem('bridgeState'));
-                    if (state == null) {
-                        state = {
+                    var storageState = JSON.parse(localStorage.getItem('bridgeState'));
+                    if (storageState === null) {
+                        $.extend(this, {
                             accordion: {
                                 opened: null,
+                                activeCourseId: null,
                                 courseData: {}
                             }
-                        };
-                        console.log("State initialization: ", state.accordion);
+                        });
+                        console.log("State initialization...");
                     } else {
-                        console.log("State from storage: ", state.accordion);
-                        // var $a = $("#accordion")
-                        //     .find($("#accordion a[data-course-index='" + state.accordion.opened + "']"))
-                        //     .trigger('click');
-                        // console.log("Found: ", $a[0])
+                        $.extend(this, storageState);
+                        console.log("State from storage...");
+                        $("#accordion a[data-course-index='" + this.accordion.opened + "']").trigger('click');
+                        clickHandler(this.accordion.opened, this.accordion.activeCourseId, this);
                     }
-                    $.extend(this, state);
                     console.log("State loaded: ", this);
                 },
 
-                save: function(newState) {
-                    var state = {};
-                    $.extend(state, newState);
-                    localStorage.setItem('bridgeState', JSON.stringify(state));
-                    console.log("State saved: ", state);
+                save: function() {
+                    localStorage.setItem('bridgeState', JSON.stringify(this));
+                    console.debug("State saved: ", this);
                 }
             };
         }());
@@ -35,12 +38,6 @@
         bridgeState.load();
 
         var modalContentFrame = $('#modal-source-preview');
-        var typeToIcon = {
-            html: "glyphicon-list-alt",
-            problem: "glyphicon-question-sign",
-            video: "glyphicon-facetime-video",
-            other: "glyphicon-option-horizontal"
-        };
 
         // activity source preview:
         $.each(activitiesData, function (i, activity) {
@@ -57,35 +54,38 @@
             var target = $(this);
             var activeCourseIndex = target.data('course-index');
             var activeCourseId = target.data("course-id");
-            clickHandler(activeCourseIndex, activeCourseId, bridgeState);
+
+            if (activeCourseIndex !== bridgeState.accordion.opened) {
+                bridgeState.accordion.opened = activeCourseIndex;
+                bridgeState.save();
+                clickHandler(activeCourseIndex, activeCourseId, bridgeState);
+            } else {
+                bridgeState.accordion.opened = null;
+                bridgeState.save();
+            }
         });
 
         function clickHandler(activeCourseIndex, activeCourseId, state) {
             var courseData;
-            var content_panel = $("#content-panel-" + activeCourseIndex).find(".panel-body").first();
-            if (content_panel.find('ul.content-sources').length) {
-                return
-            }
-
+            var content_panel = $("#content-panel-" + activeCourseIndex + " div.panel-body");
             if (state.accordion.courseData[activeCourseIndex] !== undefined) {
+                console.log("Taking data from cache ...");
                 courseData= state.accordion.courseData[activeCourseIndex];
                 renderCourseBlocks(courseData, content_panel);
             } else {
                 var requestData = {course_id: activeCourseId};
                 $.post(internalUrls.apiSources, requestData, function (responseData) {
-                    console.log("API request...");
-                    // bridgeState.save({
-                    //     accordion: {
-                    //         opened: (activeCourseIndex != bridgeState.accordion.opened) ? activeCourseIndex : null,
-                    //         courseData: {}
-                    //     }.courseData[activeCourseIndex] = responseData
-                    // });
+                    console.log("Processing API request...");
+                    state.accordion.courseData[activeCourseIndex] = responseData;
+                    state.accordion.activeCourseId = activeCourseId;
+                    state.save();
                     renderCourseBlocks(responseData, content_panel);
                 });
             }
         }
 
         function renderCourseBlocks(courseData, container) {
+            console.log("Rendering course...");
             var sourcesList = $('<div/>').addClass('list-group');
             $.each(courseData, function (i, item) {
                 var listItem = $('<button/>')
