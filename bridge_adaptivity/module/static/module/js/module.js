1f215fa2a783368dcbed276d56b229b0b2ac6c39
wowkalucky <wowkalucky@gmail.com>
2017-09-14 00:02:16 +0300

    Fix code style

--- bridge_adaptivity/module/static/module/js/module.js
+++ bridge_adaptivity/module/static/module/js/module.js
@@ -1,7 +1,7 @@
 (function ($) {
     $(function () {
         var defaultSourceItemTitle = "No Title";
-        var modalContentFrame = $('#modal-source-preview');
+        var modalContentFrame = $("#modal-source-preview");
         var activitiesData = $.map($(".activity"), function(activityRow) {
             var $activityRow = $(activityRow);
             return {
@@ -18,7 +18,7 @@
         var bridgeState = (function() {
             return {
                 load: function() {
-                    var storageState = JSON.parse(localStorage.getItem('bridgeState'));
+                    var storageState = JSON.parse(localStorage.getItem("bridgeState"));
                     if (storageState === null) {
                         $.extend(this, {
                             accordion: {
@@ -31,14 +31,14 @@
                     } else {
                         $.extend(this, storageState);
                         console.log("State from storage...");
-                        $("#accordion a[data-course-index='" + this.accordion.opened + "']").trigger('click');
+                        $("#accordion a[data-course-index='" + this.accordion.opened + "']").trigger("click");
                         clickHandler(this.accordion.opened, this.accordion.activeCourseId, this);
                     }
                     console.log("State loaded: ", this);
                 },
 
                 save: function() {
-                    localStorage.setItem('bridgeState', JSON.stringify(this));
+                    localStorage.setItem("bridgeState", JSON.stringify(this));
                     console.debug("State saved: ", this);
                 }
             };
@@ -49,17 +49,17 @@
         // activity source preview:
         $.each(activitiesData, function (i, activity) {
             createPreviewButton(
-                activity['name'],
-                activity['source_launch_url'],
-                activity['id'],
-                $('#activity-row-' + i + ' td').last(),
+                activity["name"],
+                activity["source_launch_url"],
+                activity["id"],
+                $("#activity-row-" + i + " td").last(),
                 modalContentFrame
             )
         });
 
-        $("#accordion").find("a[data-course-id]").on('click', function () {
+        $("#accordion").find("a[data-course-id]").on("click", function () {
             var target = $(this);
-            var activeCourseIndex = target.data('course-index');
+            var activeCourseIndex = target.data("course-index");
             var activeCourseId = target.data("course-id");
 
             if (activeCourseIndex !== bridgeState.accordion.opened) {
@@ -94,45 +94,45 @@
         function renderCourseBlocks(courseData, container) {
             console.log("Rendering course...");
             var usedLtiUrls = $(".activity").data("activity-source-launch-url");
-            var sourcesList = $('<div/>').addClass('list-group');
+            var sourcesList = $("<div/>").addClass("list-group");
             $.each(courseData, function (i, item) {
-                var listItem = $('<button/>')
-                    .addClass('list-group-item')
-                    .attr('type', 'button')
+                var listItem = $("<button/>")
+                    .addClass("list-group-item")
+                    .attr("type", "button")
                     .appendTo(sourcesList);
-                $('<span/>')
-                    .data('toggle', 'tooltip')
-                    .attr('title', item.type)
+                $("<span/>")
+                    .data("toggle", "tooltip")
+                    .attr("title", item.type)
                     .addClass("badge pull-left glyphicon " + typeToIcon[item.type])
                     .text(" ")
-                    .css('margin-right', '5px')
+                    .css("margin-right", "5px")
                     .appendTo(listItem);
 
-                var sourceButton = $('<span/>');
+                var sourceButton = $("<span/>");
                 // if item already is used by some Activity => block and highlight:
-                if (usedLtiUrls.indexOf(item['lti_url']) !== -1) {
+                if (usedLtiUrls.indexOf(item["lti_url"]) !== -1) {
                     sourceButton
-                        .addClass('bg-info');
+                        .addClass("bg-info");
                 } else {
                     listItem
-                        .attr('data-toggle', 'modal')
-                        .attr('data-target', '#activityModal')
-                        .on('click', function (e) {
+                        .attr("data-toggle", "modal")
+                        .attr("data-target", "#activityModal")
+                        .on("click", function (e) {
                             setInitialActivityData(item);
                         });
                 }
                 // if title is empty => set default title:
-                if (!item['display_name'].length) {
+                if (!item["display_name"].length) {
                     sourceButton.text(defaultSourceItemTitle);
                 } else {
-                    sourceButton.text(item['display_name']);
+                    sourceButton.text(item["display_name"]);
                 }
                 sourceButton
                     .appendTo(listItem);
                 createPreviewButton(
-                    item['display_name'],
-                    item['lti_url'],
-                    item['id'],
+                    item["display_name"],
+                    item["lti_url"],
+                    item["id"],
                     listItem,
                     modalContentFrame
                 )
@@ -141,22 +141,22 @@
         }
 
         function createPreviewButton(title, ltiUrl, sourceId, parent, modalFrame) {
-            var preview = $('<a/>')
+            var preview = $("<a/>")
                 .addClass("pull-right")
-                .attr('data-toggle', 'modal')
-                .attr('data-target', '#sourceModal')
+                .attr("data-toggle", "modal")
+                .attr("data-target", "#sourceModal")
                 .appendTo(parent);
-            var previewButton = $('<button/>', {'class': 'btn btn-default btn-sm'})
+            var previewButton = $("<button/>", {"class": "btn btn-default btn-sm"})
                 .appendTo(preview);
-            $('<span/>')
+            $("<span/>")
                 .addClass("glyphicon glyphicon-eye-open")
-                .attr('data-display-name', title)
-                .attr('data-lti-url', ltiUrl)
+                .attr("data-display-name", title)
+                .attr("data-lti-url", ltiUrl)
                 .appendTo(previewButton);
-            previewButton.on('click', function (e) {
+            previewButton.on("click", function (e) {
                 e.stopImmediatePropagation();
-                $('#sourceModal').modal('show');
-                $('#sourceModalLabel').text(title);
+                $("#sourceModal").modal("show");
+                $("#sourceModalLabel").text(title);
                 configurePreview(title, ltiUrl, sourceId, modalFrame);
             })
         }
@@ -167,17 +167,17 @@
             var ltiUrlParam = "source_lti_url=" + ltiUrl + "&";
             var previewUrl = internalUrls.ltiSourcePreview + "?" + idParam + displayNameParam + ltiUrlParam;
             modalFrame
-                .attr('src', previewUrl)
-                .attr('title', title)
-                .attr('name', sourceId);
+                .attr("src", previewUrl)
+                .attr("title", title)
+                .attr("name", sourceId);
         }
 
         function setInitialActivityData(source) {
             // prepopulate Activity creation modal form:
-            $('#id_name').val(source['display_name']);
-            $('#id_source_name').val(source['display_name']);
-            $('#id_source_launch_url').val(source['lti_url']);
-            $('#id_source_context_id').val(source['context_id']);
+            $("#id_name").val(source["display_name"]);
+            $("#id_source_name").val(source["display_name"]);
+            $("#id_source_launch_url").val(source["lti_url"]);
+            $("#id_source_context_id").val(source["context_id"]);
         }
     });
 }(jQuery));
