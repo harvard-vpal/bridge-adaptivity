9a899b92ab49827c6cfc88499a01e6949b5b98ca
wowkalucky <wowkalucky@gmail.com>
2017-09-14 17:01:47 +0300

    Update jquery version to 3.2.1

--- bridge_adaptivity/bridge_lti/static/bridge_lti/js/bridge-lti.js
+++ bridge_adaptivity/bridge_lti/static/bridge_lti/js/bridge-lti.js
@@ -5,7 +5,7 @@
         if ($element) {
             $element.submit();
         } else {
-            console.error("Launch form element for content preview can't be found!");
+            console.error("Launch form for content preview can't be found!");
         }
     });
 }(jQuery));
