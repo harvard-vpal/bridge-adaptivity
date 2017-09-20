3b1f0d505ec223ac182263c9e236e0dce33768fd
wowkalucky <wowkalucky@gmail.com>
2017-09-20 10:27:30 +0300

    Prevent double forms submission

--- bridge_adaptivity/module/static/module/js/module.js
+++ bridge_adaptivity/module/static/module/js/module.js
@@ -192,5 +192,24 @@
             }, 2000)
         });
 
+        // jQuery plugin to prevent double submission of forms
+        jQuery.fn.preventDoubleSubmission = function () {
+            $(this).on('submit', function (e) {
+                var $form = $(this);
+
+                if ($form.data('submitted') === true) {
+                    // Previously submitted - don't submit again
+                    e.preventDefault();
+                } else {
+                    // Mark it so that the next submit can be ignored
+                    $form.data('submitted', true);
+                }
+            });
+
+            // Keep chainability
+            return this;
+        };
+
+        $('form').preventDoubleSubmission();
     });
 }(jQuery));
