cfb73f9539ed62b9cccc76f2d5ddab5203b938b9
wowkalucky <wowkalucky@gmail.com>
2017-09-20 15:34:32 +0300

    Update prod configuration

--- bridge_adaptivity/prod_run.sh
+++ bridge_adaptivity/prod_run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 python manage.py migrate --settings=config.settings.base
+python manage.py collectstatic -v 3 -c --noinput --settings=config.settings.base
 sleep 5 && /usr/local/bin/gunicorn config.wsgi:application -w 2 -b :8000 --log-file=/dev/stdout --log-level=info --access-logfile=/dev/stdout
