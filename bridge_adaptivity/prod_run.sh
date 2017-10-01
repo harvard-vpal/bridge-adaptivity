a203f85c1a9a061a6dbe1e404b776f0a511d3edf
Andrew Ang <ang.kunanit@gmail.com>
2017-10-01 01:29:40 -0400

    Prod settings file

--- bridge_adaptivity/prod_run.sh
+++ bridge_adaptivity/prod_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python manage.py migrate --settings=config.settings.base
-python manage.py collectstatic -v 3 -c --noinput --settings=config.settings.base
+python manage.py migrate --settings=config.settings.prod
+python manage.py collectstatic -v 3 -c --noinput --settings=config.settings.prod
 sleep 5 && /usr/local/bin/gunicorn config.wsgi:application -w 2 -b :8000 --log-file=/dev/stdout --log-level=info --access-logfile=/dev/stdout
