80ab9c0158e013b17a3b5661c5cd9480ce720590
Andrew Ang <ang.kunanit@gmail.com>
2017-10-05 18:58:03 -0400

    Add health check endpoint

--- bridge_adaptivity/config/urls.py
+++ bridge_adaptivity/config/urls.py
@@ -4,11 +4,14 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 from django.views.generic import RedirectView
+from . import views
+
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^health/$', views.health),
 
     url(r'^$', login_required(RedirectView.as_view(pattern_name='module:collection-list')), name='index'),
     url(r'^lti/', include('bridge_lti.urls', namespace="lti")),
