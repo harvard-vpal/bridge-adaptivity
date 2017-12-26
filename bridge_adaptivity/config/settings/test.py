96e33bdd2d715d434652a2f79d0851f9766ccef2
Alex <django.develop@gmail.com>
2017-12-26 20:00:30 +0200

    Changes travis DB to use PG

--- bridge_adaptivity/config/settings/test.py
+++ bridge_adaptivity/config/settings/test.py
@@ -1,15 +1,26 @@
 # coding: utf-8
-
 from base import *  # noqa: F401,F403
 
 TEST_RUNNER = 'config.test_runner.PytestTestRunner'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'test.db',
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'traviscidb',
+            'USER':     'test',
+            'PASSWORD': 'password',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'test.db',
+        }
+    }
 
 SECRET_KEY = 'KEY'
 
