69b08c2e43c02cab0469398ef2105b5de68fb586
Alex <django.develop@gmail.com>
2018-01-04 19:47:39 +0200

    Try to import secure to get DB settings for tests, if fail - travis settings DB will be apply

--- bridge_adaptivity/config/settings/test.py
+++ bridge_adaptivity/config/settings/test.py
@@ -3,16 +3,20 @@ from base import *  # noqa: F401,F403
 
 TEST_RUNNER = 'config.test_runner.PytestTestRunner'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'travicidb',
-        'USER': 'test',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+try:
+    import secure
+    DATABASES = secure.DATABASES
+except ImportError:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'travicidb',
+            'USER': 'test',
+            'PASSWORD': 'password',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
 
 SECRET_KEY = 'KEY'
 
