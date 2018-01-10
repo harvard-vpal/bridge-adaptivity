20c7dcff2399206dfd3f92af285ec4edeb203bd8
Igor Degtiarov <igor.degtiarov@raccoongang.com>
2018-01-10 19:42:37 +0200

    Fix travis - local tests settings

--- bridge_adaptivity/config/settings/test.py
+++ bridge_adaptivity/config/settings/test.py
@@ -3,14 +3,16 @@ from base import *  # noqa: F401,F403
 
 TEST_RUNNER = 'config.test_runner.PytestTestRunner'
 
+UPDATE_DATABASE = {'NAME': 'traviscidb'}
+
 try:
     import secure
-    DATABASES = secure.DATABASES
 except ImportError:
-    import secure_example
-    DATABASES = secure_example.DATABASES
+    import secure_example as secure
+    UPDATE_DATABASE['HOST'] = 'localhost'
 
-DATABASES['default'].update({'NAME': 'traviscidb', 'HOST': 'localhost'})
+DATABASES = secure.DATABASES
+DATABASES['default'].update(UPDATE_DATABASE)
 
 SECRET_KEY = 'KEY'
 
