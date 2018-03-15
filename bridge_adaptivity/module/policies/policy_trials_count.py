e0276717419db57fb6c13529a12050961cd8d7f9
Alex <django.develop@gmail.com>
2018-03-15 18:41:23 +0200

    fix(AD-133): If threshold is not required by policy don't show it in group-list view

--- bridge_adaptivity/module/policies/policy_trials_count.py
+++ bridge_adaptivity/module/policies/policy_trials_count.py
@@ -3,6 +3,9 @@ from .base import BaseGradingPolicy
 
 class TrialsCountGradingPolicy(BaseGradingPolicy):
     public_name = 'Trials count'
+    require = {
+        'threshold': True
+    }
 
     summary_text = """ Overall score is the number of attempts made divided by the threshold Q, or 1 if the number of
     attempts made is greater than Q."""
