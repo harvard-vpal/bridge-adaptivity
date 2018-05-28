7cafd5ea6d6ecc3cd218f4e1d893ff5e723d402c
Yura Braiko <yura.braiko@raccoongang.com>
2018-05-28 14:18:54 +0300

    fix celery and update urls

--- bridge_adaptivity/config/urls.py
+++ bridge_adaptivity/config/urls.py
@@ -13,18 +13,18 @@ from . import views
 
 
 urlpatterns = [
-    path(r'^login/$', auth_views.login, name='login'),
-    path(r'^logout/$', auth_views.logout, name='logout'),
-    path(r'^admin/', admin.site.urls),
-    path(r'^health/$', views.health),
+    path(r'login/', auth_views.login, name='login'),
+    path(r'logout/', auth_views.logout, name='logout'),
+    path(r'admin/', admin.site.urls),
+    path(r'health/', views.health),
 
-    path(r'^$', login_required(RedirectView.as_view(pattern_name='module:collection-list')), name='index'),
-    path(r'^lti/', include(lti)),
-    path(r'^module/', include(module)),
-    path(r'^api/', include(api)),
+    path(r'', login_required(RedirectView.as_view(pattern_name='module:collection-list')), name='index'),
+    path(r'lti/', include(lti)),
+    path(r'module/', include(module)),
+    path(r'api/', include(api)),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns = [path(r'^__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
+    urlpatterns = [path(r'__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
