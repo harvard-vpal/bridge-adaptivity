e17cc8da7a25b6d2255a2c7c1021678014227ed7
Yura [flying_pi] Braiko <fp.flyingpi@gmail.com>
2018-05-30 15:30:26 +0300

    Update urls.py

--- bridge_adaptivity/config/urls.py
+++ bridge_adaptivity/config/urls.py
@@ -12,15 +12,15 @@ from . import views
 
 
 urlpatterns = [
-    path(r'login/', auth_views.login, name='login'),
-    path(r'logout/', auth_views.logout, name='logout'),
-    path(r'admin/', admin.site.urls),
-    path(r'health/', views.health),
+    path('login/', auth_views.login, name='login'),
+    path('logout/', auth_views.logout, name='logout'),
+    path('admin/', admin.site.urls),
+    path('health/', views.health),
 
-    path(r'', login_required(RedirectView.as_view(pattern_name='module:collection-list')), name='index'),
-    path(r'lti/', include(lti)),
-    path(r'module/', include(module)),
-    path(r'api/', include(api)),
+    path('', login_required(RedirectView.as_view(pattern_name='module:collection-list')), name='index'),
+    path('lti/', include(lti)),
+    path('module/', include(module)),
+    path('api/', include(api)),
 ]
 
 if settings.DEBUG:
