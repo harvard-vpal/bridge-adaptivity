a319316bff3d2ff9cd4c2db6cb1bab7b5c74f5ff
Yura Braiko <yura.braiko@raccoongang.com>
2018-05-31 18:34:00 +0300

    fix

--- bridge_adaptivity/bridge_lti/urls.py
+++ bridge_adaptivity/bridge_lti/urls.py
@@ -5,6 +5,6 @@ from bridge_lti.provider import lti_launch
 
 urlpatterns = ([
     url(r'^launch/?(?:/collection/(?P<collection_id>\d+)/group/(?P<group_slug>[\w-]+)/?)'
-        r'(?:unique_marker/(?P<unique_marker>[\w]+)/?)?$', lti_launch, name='launch'),
+        r'(?:unique_marker/(?P<unique_marker>[\w]+)/?)$', lti_launch, name='launch'),
     url(r'^source/$', source_preview, name='source-preview'),
 ], 'lti')
