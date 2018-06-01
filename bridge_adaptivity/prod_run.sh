a303f02604a77bb8baf116ee7a8010872fe525fc
Yura [flying_pi] Braiko <fp.flyingpi@gmail.com>
2018-06-01 13:03:37 +0300

    Update prod_run.sh

--- bridge_adaptivity/prod_run.sh
+++ bridge_adaptivity/prod_run.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate --settings=config.settings.prod
 python manage.py collectstatic -v 3 -c --noinput --settings=config.settings.prod
-sleep 5 && /usr/local/bin/gunicorn config.wsgi:application -w 2 -b :8000 --log-file=/dev/stdout --log-level=info --access-logfile=/dev/stdout
+sleep 5 && /usr/local/bin/gunicorn config.wsgi:application -w 2 -b :8000
