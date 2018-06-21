6a524fcd739a2559fb2fb259b9a16116ef81f1fa
Igor Degtiarov <igor.degtiarov@raccoongang.com>
2018-06-21 14:52:26 +0300

    Merge branch 'master' into idegtiarov/activity_repetition
    # Conflicts:
    #	bridge_adaptivity/bridge_lti/provider.py

--- bridge_adaptivity/module/views.py
+++ bridge_adaptivity/module/views.py
@@ -22,8 +22,8 @@ from module import tasks, utils
 from module.base_views import BaseCollectionView, BaseCourseView, BaseGroupView
 from module.forms import ActivityForm, AddCollectionGroupForm, AddCourseGroupForm, BaseGradingPolicyForm, GroupForm
 from module.mixins.views import (
-    BackURLMixin, CollectionSlugToContextMixin, GroupEditFormMixin, JsonResponseMixin, LinkObjectsMixin, LtiSessionMixin,
-    OnlyMyObjectsMixin, SetUserInFormMixin
+    BackURLMixin, CollectionSlugToContextMixin, GroupEditFormMixin, JsonResponseMixin, LinkObjectsMixin,
+    LtiSessionMixin, OnlyMyObjectsMixin, SetUserInFormMixin
 )
 from module.models import (
     Activity, Collection, CollectionGroup, Course, GRADING_POLICY_NAME_TO_CLS, Log, Sequence, SequenceItem
