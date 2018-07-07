eb9b10fd051b379e3cd6f60a3d5b5e1654e744b1
Yura Braiko <yura.braiko@raccoongang.com>
2018-07-07 17:28:14 +0300

    fix styles

--- bridge_adaptivity/module/tests/test_views.py
+++ bridge_adaptivity/module/tests/test_views.py
@@ -230,7 +230,6 @@ class CollectionGroupEditGradingPolicyTest(BridgeTestCase):
 
     def check_update_group(self, data):
         url = reverse('module:group-change', kwargs={'group_slug': self.test_cg.slug})
-        response = self.client.post(url, data=data)
         grading_policy_count = GradingPolicy.objects.all().count()
 
         self.assertEqual(grading_policy_count, GradingPolicy.objects.all().count())
@@ -387,8 +386,6 @@ class TestCourseViews(BridgeTestCase):
             'owner': self.user.id
         }
         courses_count = Course.objects.count()
-        response = self.client.post(url, data=data)
-        new_course = Course.objects.exclude(id=self.course.id).get(**data)
         self.assertNotEqual(Course.objects.count(), courses_count)
 
     def test_update_course(self):
@@ -399,7 +396,6 @@ class TestCourseViews(BridgeTestCase):
             'owner': self.user.id
         }
         courses_count = Course.objects.count()
-        response = self.client.post(url, data=data)
         self.assertEqual(Course.objects.count(), courses_count)
         course = Course.objects.get(id=self.course.id)
         self.assertEqual(course.name, data['name'])
