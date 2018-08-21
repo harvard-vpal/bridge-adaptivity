47b6da391bb0e9c58c9dd62a128843982fb1f917
Yura Braiko <yura.braiko@raccoongang.com>
2018-08-21 11:54:00 +0300

    fix styles

--- bridge_adaptivity/module/urls.py
+++ bridge_adaptivity/module/urls.py
@@ -37,8 +37,11 @@ urlpatterns = ([
     url(r'^(?:group/(?P<group_slug>[\w-]+)/)?collection/add/$', CollectionCreate.as_view(),
         name='collection-add'),
     path('collection/<slug:slug>/change/', CollectionUpdate.as_view(), name='collection-change'),
-    re_path(r'^(?:group/(?P<group_slug>[\w-]+)/)?collection/(?P<pk>\d+)/$', CollectionDetail.as_view(),
-        name='collection-detail'),
+    re_path(
+        r'^(?:group/(?P<group_slug>[\w-]+)/)?collection/(?P<pk>\d+)/$',
+        CollectionDetail.as_view(),
+        name='collection-detail'
+    ),
     url(
         r'^(?:group/(?P<group_slug>[\w-]+)/)?collection/(?P<slug>[\w-]+)/delete/?$',
         CollectionDelete.as_view(),
