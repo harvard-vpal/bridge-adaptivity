f58533a0e5c596eb505179a74dc01dcf1854e67e
Igor Degtiarov <igor.degtiarov@raccoongang.com>
2018-11-28 13:30:23 +0200

    Upgrade Django and Requests dependencies to secure versions.
    Add style improvements.

--- bridge_adaptivity/config/urls.py
+++ bridge_adaptivity/config/urls.py
@@ -12,8 +12,8 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
     path('health/', views.health),
 
