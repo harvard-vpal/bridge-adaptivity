61904603e07aad4e88fada736f664a2d9d89c961
Andrey Lykhoman <andrey.likhoman@gmail.com>
2019-01-03 18:06:08 +0200

    add tests: session, csrf. part of LTI request test

--- bridge_adaptivity/bridge_lti/tests/test_provider.py
+++ bridge_adaptivity/bridge_lti/tests/test_provider.py
@@ -2,8 +2,9 @@ import logging
 
 from ddt import data, ddt
 from django.contrib.sessions.middleware import SessionMiddleware
+
 from django.http import HttpResponse
-from django.test import RequestFactory
+from django.test import RequestFactory, override_settings
 from django.urls import reverse
 from lti.contrib.django import DjangoToolProvider
 import mock
@@ -13,6 +14,7 @@ from bridge_lti.provider import learner_flow
 from module.models import Sequence
 from module.tests.test_views import BridgeTestCase
 
+from django.test.client import Client
 log = logging.getLogger(__name__)
 
 
@@ -125,3 +127,121 @@ class ProviderTest(BridgeTestCase):
 
         # Ensure that only one LTI user was created.
         self.assertEqual(LtiUser.objects.all().count(), count_of_lti_users + 1)
+
+    @mock.patch('bridge_lti.provider.instructor_flow')
+    @mock.patch('bridge_lti.provider.learner_flow')
+    @data('Instructor',)
+    def test_lti_launch_correct_query(
+        self, role,  mock_learner_flow, mock_instructor_flow,
+    ):
+        # Checking only list of property or use validator?
+        # @mock.patch('bridge_lti.validator.SignatureValidator.validate_access_token')
+        # @mock.patch('bridge_lti.validator.SignatureValidator.validate_timestamp_and_nonce')
+        # @data('Instructor', 'Administrator', 'Learner', 'Student')
+        
+        mock_instructor_flow.return_value = HttpResponse(status=200)
+        mock_learner_flow.return_value = HttpResponse(status=200)
+
+        # 'collection_slug': self.collection1.slug,
+        # 'group_slug': str(self.test_cg.slug),
+        
+        consumer_prams = {
+            'consumer_key': self.lti_provider.consumer_key,
+            'consumer_secret': self.lti_provider.consumer_secret,
+            'launch_url': 'http://localhost' + reverse(
+                'lti:launch',
+                kwargs={
+                    'collection_slug': self.collection1.slug,
+                    'group_slug': str(self.test_cg.slug),
+
+                }),
+            'params': {
+                # Required parameters
+                'lti_message_type': 'basic-lti-launch-request',
+                'lti_version': 'LTI-1p0',
+                'resource_link_id': '-881cb88b68e34194959b3038cd5f918f',
+                # Recommended parameters
+                'user_id': 'bridge_user',
+                'roles': role,
+                'oauth_callback': 'about:blank',
+                'context_id': 'bridge_collection'
+            },
+        }
+        from lti import ToolConfig, ToolConsumer
+        consumer = ToolConsumer(**consumer_prams)
+        data = consumer.generate_launch_data()
+        response = self.client.post(
+            consumer.launch_url,
+            data=data,
+            headers={
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+
+        )
+        # mock_instructor_flow.assert_called_once_with(mock.ANY, collection_slug=mock_collection_slug)
+        # mock_learner_flow.assert_not_called()
+
+    @data('Lax', None)
+    def test_lti_launch_csrf_token(
+        self, csrf_cookie_samsite,
+    ):
+        with override_settings(CSRF_COOKIE_SAMESITE=csrf_cookie_samsite):
+            csrf_client = Client(enforce_csrf_checks=True)
+            csrf_client.cookies.clear()
+            response_get = csrf_client.get(reverse('login'))
+            cookies_item = {}
+            for key, value in response_get.cookies.items():
+                cookies_item[key] = value
+            csrftoken = cookies_item['csrftoken']
+            csrftoken_id, samesite = csrftoken.coded_value, csrftoken.get('samesite', None)
+            
+            self.assertEqual(samesite, csrf_cookie_samsite or '')
+            
+            response_status = 302
+            if samesite:
+                csrf_client.cookies.clear()
+                response_status = 403
+            response = csrf_client.post(
+                reverse(
+                    'login'
+                ),
+                data={
+                    'username': 'test',
+                    'password': "test",
+                    'csrfmiddlewaretoken': csrftoken_id
+                }
+            )
+            self.assertEqual(response.status_code, response_status)
+
+    @mock.patch('bridge_lti.provider.get_tool_provider_for_lti')
+    @mock.patch('bridge_lti.provider.learner_flow')
+    @mock.patch('module.views.SequenceComplete.get_object')
+    @data('lax', None)
+    def test_lti_launch_session_check(
+        self, session_cookie_samsite, mock_module_get_object, mock_learner_flow, mock_get_tool_provider_for_lti
+    ):
+        mock_get_tool_provider_for_lti.return_value = True
+        mock_learner_flow.return_value = HttpResponse(status=200)
+        mock_module_get_object.__name__ = 'get_object'
+        mock_collection_slug = '3'
+        with override_settings(SESSION_COOKIE_SAMESITE=session_cookie_samsite):
+            self.client.post(
+                reverse(
+                    'lti:launch',
+                    kwargs={
+                        'collection_slug': mock_collection_slug,
+                        'group_slug': 'group-slug',
+                    }
+                ),
+                data={
+                    'oauth_nonce': 'oauth_nonce',
+                    'oauth_consumer_key': self.lti_provider.consumer_key,
+                }
+            )
+            response_status = 200
+            if session_cookie_samsite:
+                # delete session. It's the same if we send get query without sessionid and cookies.
+                self.client.session.flush()
+                response_status = 403
+            response = self.client.get(reverse('module:sequence-complete', kwargs={'pk': mock_collection_slug}))
+            self.assertEqual(response.status_code, response_status)
