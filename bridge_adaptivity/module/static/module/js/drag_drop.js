a10c23d23259b15165324c2df0fc0ebfc2cca6c1
Andrey Lykhoman <andrey.likhoman@gmail.com>
2019-01-14 16:01:38 +0200

    fix comments in js methods

--- bridge_adaptivity/module/static/module/js/drag_drop.js
+++ bridge_adaptivity/module/static/module/js/drag_drop.js
@@ -2,6 +2,6 @@
 
 function set_event_data_transfer_from_event_target_dataset(event_data_transfer, event_target_dataset) {
     // Function that work with event.dataTransfer and event_target_dataset. Example:
-    //  event.dataTransfer.setData("text/plain", event.target.dataset.index + ',' + event.target.dataset.move_url);
+    //  event_data_transfer.setData("text/plain", event_target_dataset.index + ',' + event_target_dataset.move_url);
     event_data_transfer.setData("text/plain", event_target_dataset.index + ',' + event_target_dataset.move_url)
 }
