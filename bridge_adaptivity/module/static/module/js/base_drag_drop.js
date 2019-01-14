0fbca783e70b0dab1c4182516a21dd6afaf7ba49
Andrey Lykhoman <andrey.likhoman@gmail.com>
2019-01-14 12:42:52 +0200

    refactoring drag and drop js.

---
+++ bridge_adaptivity/module/static/module/js/base_drag_drop.js
@@ -0,0 +1,83 @@
+// Utility function which exchange element's index
+function updateElement(element, index) {
+    element.dataset.index = index;
+    return element;
+}
+
+
+function before_add_element(element, index) {
+    //Write script that will run before add ellement to table
+}
+
+function is_forbidden_to_chage(dataset, data) {
+    return false
+}
+
+function get_ordering_elements_list(moveIndex, elIndex) {
+    let collectionList = $('tr.droppable'),
+        shift = 0,
+        newCollectionList = [];
+    $.each(collectionList, function (index, _) {
+            if (index == elIndex) {
+                newCollectionList[index] = updateElement(collectionList[moveIndex], index);
+                shift = moveIndex < elIndex ? 0 : -1;
+            } else if (index == moveIndex && moveIndex < elIndex) {
+                shift = 1;
+                newCollectionList[index] = updateElement(collectionList[index + shift], index);
+            } else if (index == moveIndex && moveIndex > elIndex) {
+                newCollectionList[index] = updateElement(collectionList[index + shift], index);
+                shift = 0;
+            } else {
+                newCollectionList[index] = updateElement(collectionList[index + shift], index);
+            }
+        });
+    collectionList.remove();
+    return newCollectionList
+}
+
+// Function processing drop of the element
+function drop_handler(event, el) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("text/plain").split(','),
+        elIndex = el.dataset.index, // index of the target element
+        moveUrl = data[1], // move URL of the dropped element
+        moveIndex = data[0], // initial index of the dropped element
+    is_forbidden_extra_cheack = is_forbidden_to_chage(el.dataset, data)
+    if (moveIndex === elIndex || is_forbidden_extra_cheack) {
+        console.log("Item doesn't change the order");
+    } else {
+        console.log("Trying change Item " + moveIndex + " order to " + elIndex);
+        moveUrl = moveUrl.replace("?", elIndex + "?");
+        orderingElementsList = get_ordering_elements_list(moveIndex, elIndex)
+        $.each(orderingElementsList, function (index, element) {
+            before_add_element(element, index)
+            $("table").append(element);
+        });
+        $.ajax({
+            type: "GET",
+            url: moveUrl,
+            success: responseData => {
+                console.log("Collection's changed order is persisted to the backend.");
+            },
+            error: responseData => console.log("Cannot change collections order " + responseData)
+        });
+    }
+}
+
+function dragover_handler(event, drop_effect="move") {
+    event.preventDefault();
+    // Set the dropEffect to move
+    event.dataTransfer.dropEffect = drop_effect;
+}
+
+
+function set_event_data_transfer_from_event_target_dataset(event_data_transfer, event_target_dataset) {
+    // Function that work with event.dataTransfer and event_target_dataset. Example:
+    //  event.dataTransfer.setData("text/plain", event.target.dataset.index + ',' + event.target.dataset.move_url);
+}
+
+// Handler function of the drag start process
+function dragstart_handler(event, drop_effect="move") {
+    set_event_data_transfer_from_event_target_dataset(event.dataTransfer, event.target.dataset)
+    event.dropEffect = drop_effect;
+}
--- bridge_adaptivity/module/static/module/js/collection_drag_drop.js
+++ bridge_adaptivity/module/static/module/js/collection_drag_drop.js
@@ -1,65 +1,25 @@
-// Utility function which exchange element's index
-function updateElement(element, index) {
-    element.dataset.index = index;
-    return element;
-}
+// include base_drag_drop.js before this script
 
-// Function processing drop of the element
-function drop_handler(event, el) {
-    event.preventDefault();
-    let data = event.dataTransfer.getData("text/plain").split(','),
-        elIndex = el.dataset.index, // index of the target element
-        elAtype = el.dataset.atype, // atype of the target element
-        moveUrl = data[1], // move URL of the dropped element
-        moveIndex = data[0], // initial index of the dropped element
-        moveAtype = data[2]; // atype of the dropped element
-    if (moveIndex === elIndex || moveAtype !== elAtype || elAtype === "G" || moveAtype === "G" ) {
-        console.log("Item doesn't change the order");
-    } else {
-        console.log("Trying change Item " + moveIndex + " order to " + elIndex);
-        moveUrl = moveUrl.replace("?", elIndex + "?");
-        let collectionList = $('tr.droppable'),
-            shift = 0,
-            newCollectionList = [];
-        // Update elements order on the frontend
-        $.each(collectionList, function (index, _) {
-            if (index == elIndex) {
-                newCollectionList[index] = updateElement(collectionList[moveIndex], index);
-                shift = moveIndex < elIndex ? 0 : -1;
-            } else if (index == moveIndex && moveIndex < elIndex) {
-                shift = 1;
-                newCollectionList[index] = updateElement(collectionList[index + shift], index);
-            } else if (index == moveIndex && moveIndex > elIndex) {
-                newCollectionList[index] = updateElement(collectionList[index + shift], index);
-                shift = 0;
-            } else {
-                newCollectionList[index] = updateElement(collectionList[index + shift], index);
-            }
-        });
-        collectionList.remove();
-        $.each(newCollectionList, function (index, element) {
-            $("table").append(element);
-        });
-        $.ajax({
-            type: "GET",
-            url: moveUrl,
-            success: responseData => {
-                console.log("Collection's changed order is persisted to the backend.");
-            },
-            error: responseData => console.log("Cannot change collections order " + responseData)
-        });
+function before_add_element(element, index) {
+    //Write script that will run before add ellement to table
+    label = element.getElementsByClassName("label");
+    if (label.length !== 0){
+        labelItem = label.item(0);
+        labelItem.innerHTML = element.getAttribute("labelstring") + index;
     }
 }
 
-// Handler function of the drag over process
-function dragover_handler(event) {
-    event.preventDefault();
-    // Set the dropEffect to move
-    event.dataTransfer.dropEffect = "move";
+function is_forbidden_to_chage(dataset, data) {
+    elAtype = dataset.atype;
+    moveAtype = data[2];
+    if (moveAtype !== elAtype || elAtype === "G" || moveAtype === "G"){
+        return true
+    }
+    return false
 }
 
-// Handler function of the drag start process
-function dragstart_handler(event) {
-    event.dataTransfer.setData("text/plain", event.target.dataset.index + ',' + event.target.dataset.move_url + ',' + event.target.dataset.atype);
-    event.dropEffect = "move";
+function set_event_data_transfer_from_event_target_dataset(event_data_transfer, event_target_dataset) {
+    // Function that work with event.dataTransfer and event_target_dataset. Example:
+    //  event.dataTransfer.setData("text/plain", event.target.dataset.index + ',' + event.target.dataset.move_url);
+    event_data_transfer.setData("text/plain", event_target_dataset.index + ',' + event_target_dataset.move_url + ',' + event_target_dataset.atype);
 }
