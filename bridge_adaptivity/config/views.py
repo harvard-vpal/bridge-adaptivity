9961f562cf4639356f32d8e06c9ce4d933d74d79
Igor Degtiarov <igoryok.d@gmail.com>
2019-01-14 14:20:16 +0200

    Read only Instructor feature (#208)
    * Read-only instructor feature

--- bridge_adaptivity/config/views.py
+++ bridge_adaptivity/config/views.py
@@ -1,5 +1,23 @@
+from logging import getLogger
+
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse
+
+log = getLogger(__name__)
 
 
 def health(request):
     return HttpResponse()
+
+
+class BridgeLoginView(LoginView):
+    """
+    OverLoad Login Class view to make available read-only mode.
+    """
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        if self.request.session.get('read_only_collection'):
+            log.debug('Read-only view is active.')
+            context['read_only_available'] = True
+        return context
