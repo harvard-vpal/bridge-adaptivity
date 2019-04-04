4f83a30ad7534c44c0335e30de9ea94cea4438ba
Andrey Lykhoman <andrey.likhoman@gmail.com>
2019-04-04 13:06:00 +0300

    fix bug in collections queryset for CollectionOrder form

--- bridge_adaptivity/module/mixins/views.py
+++ bridge_adaptivity/module/mixins/views.py
@@ -121,7 +121,8 @@ class CollectionOrderEditFormMixin:
         form = super().get_form()
         collections = (
             Collection.objects.filter(owner=self.request.user) |
-            Collection.objects.filter(collection_groups__contributors=self.request.user, collection_groups=form.group)
+            Collection.objects.filter(collection_groups__contributors=self.request.user, collection_groups=form.group)|
+            Collection.objects.filter(collection_groups__owner=self.request.user)
         )
         form.fields['engine'].initial = Engine.get_default()
         form.fields['collection'].queryset = collections.distinct()
