f32dab53455ef581a10e0f181fe49ca18791d31e
Andrey Lykhoman <andrey.likhoman@gmail.com>
2019-04-09 15:58:37 +0300

    Change fail message

--- bridge_adaptivity/module/static/module/js/consumer.js
+++ bridge_adaptivity/module/static/module/js/consumer.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
     $("#contributors .delete-contributor").on("click", function() {
-            var element = $(this);
-           $.ajax({
-              url: this.dataset["remove_url"],
-            }).done(function() {
-              element.closest("tr").remove();
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-              console.log(textStatus + "(" + errorThrown + "): " + jqXHR.responseText.split('\n')[0]);
-              element.closest("tr").children("td.error").append("<p>We fix some problems. Please, try later.</p>");
-            });
+        var element = $(this);
+        $.ajax({
+            url: this.dataset["remove_url"],
+        }).done(function() {
+            element.closest("tr").remove();
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus + "(" + errorThrown + "): " + jqXHR.responseText.split('\n')[0]);
+            element.closest("tr").children("td.error").append(
+                "<p>Cannot be removed, please try again later. " +
+                "If the problem will be procced contact with the Bridge admin.</p>"
+            );
+        });
     });
 });
